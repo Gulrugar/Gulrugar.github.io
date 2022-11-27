e987d4d69a60a35fa762b9dfb02552e445fc56ee
Andrew Aguirre <andrewa96@ymail.com>
2022-11-26 17:32:24 -0800

    testing 2

--- js/script.js
+++ js/script.js
@@ -8,11 +8,14 @@ btnButton.addEventListener('click', () => {
     switch (backgroundColor) {
         case 'white':
             body.style.backgroundColor = 'rgb(40, 40, 40)'
+            body.style.color = 'white'
             break
         case 'rgb(40, 40, 40)':
             body.style.backgroundColor = 'white'
+            body.style.color = 'rgb(40, 40, 40)'
             break
         default:
             body.style.backgroundColor = 'white'
+            body.style.color = 'rgb(40, 40, 40)'
     }
 })
