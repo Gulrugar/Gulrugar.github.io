f1b67c0f0ed3a5ef93d5afdc02d0769623386c00
Andrew Aguirre <andrewa96@ymail.com>
2023-06-16 13:15:59 -0700

    add close mobile menu on screen resize > 800px

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,19 +3,36 @@ const mobileLinks = document.querySelectorAll(".mobile-menu__link");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 // new menu
-
-mobileToggle.addEventListener("click", () => {
+function openMenu() {
   document.body.classList.toggle("mobile-open");
   document.body.classList.toggle("stop-scrolling");
-});
+}
+
+function closeMenu() {
+  document.body.classList.remove("mobile-open");
+  document.body.classList.remove("stop-scrolling");
+  mobileToggle.blur();
+}
+
+mobileToggle.addEventListener("click", openMenu);
 
 mobileLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    document.body.classList.remove("mobile-open");
-    document.body.classList.remove("stop-scrolling");
-  });
+  link.addEventListener("click", closeMenu);
 });
 
+// Mobile Menu is hidden by default so that it doesn't show on page load. This code makes it visible once the page is loaded.
 window.onload = () => {
   mobileMenu.style.display = "block";
 };
+
+// Close Mobile Menu if open on window resize > 800px
+
+const mediaQuery = window.matchMedia("(min-width: 800px)");
+
+function handleMediaQueryChange(mediaQuery) {
+  if (mediaQuery.matches) {
+    closeMenu();
+  }
+}
+
+mediaQuery.addEventListener("change", handleMediaQueryChange);
